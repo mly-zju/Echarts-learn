2e54b50d6dde52d23dcadd632b8c72d78f669b26
mly-zju <malingyang@foxmail.com>
2016-12-09 21:41:44 +0800

    world comparison2 update

--- world-comparison2/index.js
+++ world-comparison2/index.js
@@ -1,36 +1,56 @@
-var echarts=require('echarts');
-var data=require('./data.json');
-var worldJson=require('../map/world.json');
-echarts.registerMap('world',worldJson);
-var myChart=echarts.init(document.getElementById('main'));
+var echarts = require('echarts');
+var data = require('./data.json');
+var worldJson = require('../map/world.json');
+echarts.registerMap('world', worldJson);
+var myChart = echarts.init(document.getElementById('main'));
 
-var option={
+var option = {
   baseOption: {
     title: {
-        text: '大国崛起：改革开放以来中国gdp排名变化情况',
-        left: 'center',
-        top: 'top',
-        textStyle: {
-            fontSize: 25,
-            color: 'rgba(0,0,0, 0.9)'
-        }
+      text: '大国崛起：改革开放以来中国gdp排名变化情况',
+      left: 'center',
+      top: 'top',
+      textStyle: {
+        fontSize: 25,
+        color: 'rgba(0,0,0, 0.9)'
+      }
     },
-    tooltip:{},
-    visualMap:{
+    tooltip: {
+      formatter: '{b}: {c0}'
+    },
+    grid: {
+      left: 150,
+      right: 400,
+      top: '70%',
+      bottom: 20
+    },
+    xAxis: {
+      type: 'value',
+      boundaryGap: [0, 0.1]
+    },
+    yAxis: {
+      type: 'category',
+      data: data[0].data.map(function(ele) {
+        return ele.value[2]
+      }).reverse()
+    },
+    visualMap: {
       dimension: 0,
       left: 10,
       min: data[0].data[9].value[0],
       max: data[0].data[0].value[0],
-      text: ['High','Low'],
+      text: ['High', 'Low'],
       inRange: {
-           color: ['lightskyblue','yellow', 'orangered']
-       }
+        color: ['lightskyblue', 'yellow', 'orangered']
+      }
     },
     series: [
       {
         type: 'map',
         mapType: 'world',
         roam: true,
+        top: '10%',
+        bottom: '25%',
         itemStyle: {
           emphasis: {
             label: {
@@ -40,6 +60,25 @@ var option={
           }
         },
         data: data[0].data
+      }, {
+        type: 'bar',
+        tooltip: {
+          show: false
+        },
+        label: {
+          normal: {
+            show: true,
+            position: 'right',
+            textStyle: {
+              color: '#000'
+            }
+          }
+        },
+        data: data[0].data.map(function(ele, index) {
+          return ele.value[0]
+        }).sort(function(a, b) {
+          return a > b;
+        })
       }
     ]
   }
